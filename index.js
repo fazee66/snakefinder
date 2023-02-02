0c8ef6396adeba6f2fc85e2c25f90828a6b463f7
fazee66 <116349629+fazee66@users.noreply.github.com>
2023-02-02 18:11:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Do you want to find you want to find the snake percentage of ur friend");
+// prompt("What is your name?");
+var gname= prompt("type the name?");
+var loveScore=Math.random() * 100;
+loveScore=Math.floor(loveScore);
+if (loveScore > 50) {alert( gname + " is " +loveScore+ " % snake   i feel your pain");}
+else{
+   alert("your friend is " +loveScore+ " % snake,, shukr hae ap bach gae par kab tak bache ge ");
+}
